6022dd0a43b723e06f1974d32f09cf05dbb0a231
DavidGalamyan <DavidGalamyan@gmail.com>
2021-05-14 01:18:17 +0300

    ReWork Home Work, clean code

---
+++ LessonAsync/DataPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonAsync
+{
+    static class DataPost
+    {
+        private readonly static string _file = "message.txt";
+        public static void PostsWriteToFile(string[] posts)
+        {
+            File.WriteAllLinesAsync(_file, posts);
+        }
+    }
+}
---
+++ LessonAsync/PostService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonAsync
+{
+    class PostService
+    {
+        private readonly string _url = "https://jsonplaceholder.typicode.com/posts/";
+        private static HttpClient _client = new HttpClient();
+        /// <summary>
+        /// Enter PostID to get data
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
+        public async Task<PostResponse> GetPost(int postId)
+        {
+            var response = await _client.GetAsync($"{_url}/{postId}");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Что-то пошло не так");
+                return default;
+            }
+            var content = await response.Content.ReadAsStringAsync();
+            var post = JsonConvert.DeserializeObject<PostResponse>(content);
+            return post;
+        }
+    }
+}
